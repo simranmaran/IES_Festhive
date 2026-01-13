af7a6212bf67567dfc8dcddf314a3ea2f2f979b7
Simran  Maran <153758596+simranmaran@users.noreply.github.com>
2026-01-13 14:24:29 +0530

    Delete jiya/backend directory

--- jiya/backend/src/model/otp.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const otpSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  otp: { type: String, required: true },
-  expiresAt: { type: Date, required: true },
-});
-
-export default mongoose.model("OTP", otpSchema);
